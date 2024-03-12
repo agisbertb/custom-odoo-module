80cbf28648b0f233216b8d258944983e4977f40f
agisbertb <agisbert@iesebre.com>
2024-03-12 16:12:41 +0100

    Added structure files

--- reclamations/__manifest__.py
+++ reclamations/__manifest__.py
@@ -1,11 +1,17 @@
 {
-    "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0.0",  # Version
+    "name": "Reclamations",  # The name that will appear in the App list
+    "version": "1.0.0",  # Version
+    "author": "Andreu Gisbert Bel & Ismael Semmar Gàlvez", # author
+    "summary": "Customer complaints",  # A short description of the module
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
+        "security/ir.model.access.csv",
+        "views/reclamation_view.xml",
+        "views/reclamation_menu.xml",
 
     ],
+    "images": ['static/description/icon.png'],
     "installable": True,
     'license': 'LGPL-3',
 }
